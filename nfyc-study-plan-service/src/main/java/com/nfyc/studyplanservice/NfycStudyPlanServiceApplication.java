e392c95c920aa389ddba56190226655827d7cc0f
Aditya SHUKLA <aditya.shukla@outpayce.com>
2023-10-30 02:35:25 +0530

    initial project setup

---
+++ nfyc-study-plan-service/src/main/java/com/nfyc/studyplanservice/NfycStudyPlanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nfyc.studyplanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfycStudyPlanServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfycStudyPlanServiceApplication.class, args);
+    }
+
+}
---
+++ nfyc-study-plan-service/src/test/java/com/nfyc/studyplanservice/NfycStudyPlanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfyc.studyplanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfycStudyPlanServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
