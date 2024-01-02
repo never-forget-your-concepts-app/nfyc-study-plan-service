b0b4ed68af178ad29b6c71921ecd977ff01d2778
Aditya SHUKLA <aditya.shukla@outpayce.com>
2024-01-03 01:11:47 +0530

    incorporated review comments

--- src/main/java/com/nfyc/studyplanservice/config/cache/database/AzureMySQLConfiguration.java
+++ src/main/java/com/nfyc/studyplanservice/config/cache/database/AzureMySQLConfiguration.java
@@ -5,7 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 
 import javax.sql.DataSource;
 
